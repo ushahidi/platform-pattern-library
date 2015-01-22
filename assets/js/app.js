aa0645d3acef11531e9a32b41b1d3972f8c7719d
Seth Hall <seth@ushahidi.com>
2015-01-22 14:49:52 -0500

    removed app.js from .gitignore

---
+++ assets/js/app.js
@@ -0,0 +1,1 @@
+$(window).scroll(function(){var l=$(window).scrollTop();l>=130?($(".pl-jump-nav").addClass("pl-fixed"),$(".pl").addClass("sticky-nav")):($(".pl-jump-nav").removeClass("pl-fixed"),$(".pl").removeClass("sticky-nav"))});
