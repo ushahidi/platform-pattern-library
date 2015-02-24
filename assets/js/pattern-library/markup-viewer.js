40023a58175415a3bd9ea5b485d2da778593dd40
Sophie Shepherd <sophshep@gmail.com>
2015-02-24 15:43:03 -0600

    breadcrumbs & progress bar. adding code snippets and styling them.
    Summary:
    Merge branch 'gh-pages' of https://github.com/ushahidi/platform-pattern-library into progress-bar-breadcrumb
    
    Progress Bar, Breadcrumb, Code snippets
    
    Test Plan: test snippets, progress in multiple browsers. check code is clean.
    
    Reviewers: seth
    
    Reviewed By: seth
    
    Subscribers: brandonrosage, seth
    
    Differential Revision: https://phabricator.ushahidi.com/D678

--- assets/js/pattern-library/markup-viewer.js
+++ assets/js/pattern-library/markup-viewer.js
@@ -1,6 +1,6 @@
 $(function() {
 	$('.pl-sub-pattern-markup').each(function(){
-		$(this).after('<div class="pl-sub-pattern-sample"><div class="pl-sub-pattern-sample-toggle-controls"><span class="show">&#43; Show</span><span class="hide">&#45; Hide</span> HTML</div><div class="pl-sub-pattern-sample-toggle-target"><textarea>'+$(this).html()+'</textarea></div></div>');
+		$(this).before('<div class="pl-sub-pattern-sample"><div class="pl-sub-pattern-sample-toggle-controls"><span class="show">&#43; Show</span><span class="hide">&#45; Hide</span> HTML</div><div class="pl-sub-pattern-sample-toggle-target"><textarea>'+$(this).html()+'</textarea></div></div>');
 	});
 
 	$('.pl-sub-pattern').on('click','.pl-sub-pattern-sample-toggle-controls', function(){
