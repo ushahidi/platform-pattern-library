c73c7953bf01d07919bf1fbd6463055a8c283b97
Seth Hall <seth@ushahidi.com>
2015-03-02 10:54:52 -0500

    PrismJS
    Summary:
    * first pass at syntax highllighting
    
    * ideally, we still need to be ablt to correctly indent html then remove spaces before each line.
    
    Fixes T1381
    
    Test Plan:
    * toggle "+ SHOW HTML"
    * check syntax highlighting
    * review code
    * provide feedback about theme
    
    Reviewers: sophshep
    
    Reviewed By: sophshep
    
    Subscribers: brandonrosage
    
    Maniphest Tasks: T1381
    
    Differential Revision: https://phabricator.ushahidi.com/D692

--- assets/js/pattern-library/markup-viewer.js
+++ assets/js/pattern-library/markup-viewer.js
@@ -1,6 +1,6 @@
 $(function() {
 	$('.pl-sub-pattern-markup').each(function(){
-		$(this).before('<div class="pl-sub-pattern-sample"><div class="pl-sub-pattern-sample-toggle-controls"><span class="show">&#43; Show</span><span class="hide">&#45; Hide</span> HTML</div><div class="pl-sub-pattern-sample-toggle-target"><textarea>'+$(this).html()+'</textarea></div></div>');
+		$(this).before('<div class="pl-sub-pattern-sample"><div class="pl-sub-pattern-sample-toggle-controls"><span class="show">&#43; Show</span><span class="hide">&#45; Hide</span> HTML</div><div class="pl-sub-pattern-sample-toggle-target"><pre class="language-markup"><code><script type="prism-html-markup">'+$(this).html()+'</script></code></pre></div></div>');
 	});
 
 	$('.pl-sub-pattern').on('click','.pl-sub-pattern-sample-toggle-controls', function(){
