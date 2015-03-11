0ac80179a758a42154111ecbd77848874d392c2c
Seth Hall <seth@ushahidi.com>
2015-03-11 15:28:58 -0400

    side nav sub nav
    Summary: adds dropdown/sub menu support in main nav
    
    Test Plan:
    * check main nav sub nav on all screen sizes
    * check code for different approach
    
    Reviewers: sophshep
    
    Reviewed By: sophshep
    
    Subscribers: brandonrosage
    
    Differential Revision: https://phabricator.ushahidi.com/D716

--- assets/js/custom/main-nav.js
+++ assets/js/custom/main-nav.js
@@ -1,3 +1,8 @@
-if ( $('.navigation').height() > $(window).height() ) {
+if ( $('.navigation:not(.pl-sub-pattern .navigation)').height() > $(window).height() ) {
     $('.navigation').toggleClass('absolute');
 }
+
+$('.hidden-content-trigger').click(function() {
+	$(this).toggleClass('open');
+    $(this).siblings('.hidden-content').toggleClass('visible');
+});
