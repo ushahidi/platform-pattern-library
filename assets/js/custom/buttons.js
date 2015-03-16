b7e28df3981f86576e7d05013e03607a17381f8e
Seth Hall <seth@ushahidi.com>
2015-03-16 11:23:08 -0500

    post list actions
    Summary: Responsive post list actions and dropdown
    
    Test Plan:
    * this is a mini diff so the thinking/process can be reviewed
    * should the 1st level dropdowns display inline?
    
    Reviewers: sophshep
    
    Reviewed By: sophshep
    
    Differential Revision: https://phabricator.ushahidi.com/D719

--- assets/js/custom/buttons.js
+++ assets/js/custom/buttons.js
@@ -1,9 +1,10 @@
 $('.toggle-js').click(function() {
 	$(this).toggleClass('open');
-    $(this).parent().children('.toggle-content').toggleClass('visible');
+    $(this).parent().siblings('.toggle-content').toggleClass('visible');
+	$(this).parent().find('.toggle-content').toggleClass('visible');
 });
 
-$('.toggle-content ul li a').click(function() {
+$('.toggle-content-secondary ul li a').click(function() {
     $(this).closest('.toggle-content').removeClass('visible');
-    $(this).parents('ul, li, .toggle-content').siblings('.toggle-js').removeClass('visible');
+    $(this).parents('ul, li, .toggle-content').siblings('.toggle-js').removeClass('open');
 });
