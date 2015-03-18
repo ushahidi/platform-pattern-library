91cc5fef27112794e15a3fe813565ba31fda9cf1
Brandon Rosage <brandon.rosage@gmail.com>
2015-03-18 15:20:57 -0500

    init Parallax
    Summary:
    Signed-off-by: Brandon Rosage <brandon.rosage@gmail.com>
    
    Scrolling transition + animation stable
    
    Signed-off-by: Brandon Rosage <brandon.rosage@gmail.com>
    
    Test Plan: Review transition between states and JavaScript structure
    
    Reviewers: seth, sophshep
    
    Reviewed By: sophshep
    
    Differential Revision: https://phabricator.ushahidi.com/D728

--- assets/js/custom/dynamic-header.js
+++ assets/js/custom/dynamic-header.js
@@ -1,24 +1,65 @@
-$('.nav-icon-js').click(function(){
-	$(this).toggleClass('open');
-	$(this).parent().find('.button-create').toggleClass('fade-out');
+headerScrollReact = function(headerHeight, lastScrollTop, parallaxReady) {
+    // When scrolling, fade content relative to scroll position and speed
+    $(window).scroll(function(event){
+       var scrollAmt = $(this).scrollTop();
 
-	var findParent = $(this).parent().parent('header').siblings('.wrapper');
+       if ((scrollAmt >= headerHeight) && (scrollAmt < headerHeight + 75)) {
+            // TRANSITION
+            $('.header').removeClass('header-full header-compact');
+            if (scrollAmt > lastScrollTop){
+                // Scrolling down
+                $('.header').delay(1500).addClass('header-transition');
+            } else {
+                // Scrolling up
+                $('.header').addClass('header-transition');
+            }
+       } else if (scrollAmt >= (headerHeight + 75)) {
+            // COMPACT
+            $('.header').removeClass('header-transition header-full').addClass('header-compact');
+       } else {
+            // FULL
+            $('.header').removeClass('header-transition header-compact').addClass('header-full');
 
-	findParent.find('.navigation').toggleClass('open');
-	findParent.find('.page-content').toggleClass('slide');
-});
-
-$(function() {
-    //caches a jQuery object containing the header element
-    var header = $('.has-dynamic-header .header');
-
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-
-        if (scroll >= 350) {
-            header.removeClass('header-full').addClass('header-compact');
-        } else {
-            header.removeClass('header-compact').addClass('header-full');
-        }
+            if (parallaxReady == true) {
+                var deltaS = scrollAmt - lastScrollTop;
+                $('.parallax').css({
+                    bottom: "-=" + deltaS/4.5,
+                    opacity: "-=" + deltaS/200
+                });
+                lastScrollTop = scrollAmt;
+            }
+       }
     });
+}
+
+$(document).ready(function(){
+    var headerHeight = $('.header-full').outerHeight(),
+        lastScrollTop = $(window).scrollTop();
+
+        $('.parallax').hide().fadeIn(400);
+
+    // If page loads with scroll position greater than 0
+    if (lastScrollTop != 0) {
+        $('.header').removeClass('header-transition header-full').addClass('header-compact');
+        headerScrollReact(headerHeight, lastScrollTop, false);
+/*
+        var delta = headerHeight - lastScrollTop;
+        $('.parallax').css({
+            bottom: delta/4.5,
+            opacity: delta/200
+        });
+*/
+    } else {
+        $('.header').height(headerHeight);
+        headerScrollReact(headerHeight, lastScrollTop, true);
+    }
+
+    $('.nav-icon-js').click(function(){
+        $('body').toggleClass('navigation-open');
+    });
+
+    $('.button-toggle').click(function() {
+        $(this).toggleClass('open');
+    });
+
 });
--- assets/js/custom/main-nav.js
+++ assets/js/custom/main-nav.js
@@ -1,3 +1,4 @@
-if ( $('.navigation:not(.pl-sub-pattern .navigation)').height() > $(window).height() ) {
-    $('.navigation').toggleClass('absolute');
-}
+$('.hidden-content-trigger').click(function() {
+	$(this).toggleClass('open');
+    $(this).parents().siblings('.hidden-content').toggleClass('visible');
+});
