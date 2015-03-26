5c2e45f9e5f8b7e80021c660df6d6739af5292bd
Seth Hall <seth@ushahidi.com>
2015-03-26 12:51:42 -0400

    cleans up main-nav.js
    Summary: fixes T1472
    
    Test Plan: make sure main-nav.js is still working
    
    Reviewers: sophshep
    
    Reviewed By: sophshep
    
    Maniphest Tasks: T1472
    
    Differential Revision: https://phabricator.ushahidi.com/D744

--- assets/js/custom/main-nav.js
+++ assets/js/custom/main-nav.js
@@ -1,27 +1,21 @@
+// this removes any active classes then adds the active class to clicked element
 $('.main-nav').find('li a').click(function(e) {
     $('.main-nav').find('li a').removeClass('active');
     $(this).addClass('active');
 });
 
+// after page refresh add classes if href matches url
 var url = window.location.href;
 
 $('.main-nav li a').filter(function() {
-    return this.href == url;
-}).addClass('active');
+    if (this.href == url) {
+        $(this).addClass('active');
+    }
+});
 
-var pageName = (location.pathname).split('/').pop();
-
-$('.main-nav li .hidden-content').find('li a').each(function(index, value)
-{
-    // Append a '$' to the pagename to make the match()-function search
-    // from the end of the href value:
-    pageName += '$';
-
-    if( value.href.match(pageName))
-    {
-        // If the pagename matches the href-attribute, then add classes
-        $(this).parent().parent().siblings('.trigger-container').children('.hidden-content-trigger').addClass('open active');
-
-        $(this).parent().parent().addClass('visible');
+$('.main-nav li .hidden-content').find('li a').filter(function() {
+    if (this.href == url) {
+        $(this).addClass('active');
+        $(this).parent().parent().addClass('visible').siblings('.trigger-container').children('.hidden-content-trigger').addClass('open active');
     }
 });
