f8684251c5520ba113fa87415a3d94db0707ef92
Brandon Rosage <brandon.rosage@gmail.com>
2015-10-05 16:12:36 -0500

    REVISION: Toggle and Custom Fieldset pattern
    Signed-off-by: Brandon Rosage <brandon.rosage@gmail.com>

--- assets/js/custom/custom-fieldset.js
+++ assets/js/custom/custom-fieldset.js
@@ -1,13 +1,9 @@
-customFieldset = function(){
-    $('.custom-fieldset').each(function(){
-        var context = $(this),
-            fields = $(context).children(':not(legend)'),
-            trigger = $(context).children('legend');
+$('.custom-fieldset').each(function(){
+    var context = $(this),
+        fields = $(context).children(':not(legend)'),
+        trigger = $(context).children('legend');
 
-        $(context).addClass('init dropdown-group');
-        $(fields).wrapAll('<div class="toggle-content dropdown-menu" />');
-        $(trigger).addClass('dropdown toggle-content-trigger');
-    });
-}
-
-customFieldset();
+    $(context).addClass('init dropdown-group');
+    $(fields).wrapAll('<div class="toggle-content dropdown-menu" />');
+    $(trigger).addClass('dropdown toggle-content-trigger');
+});
--- assets/js/custom/toggle.js
+++ assets/js/custom/toggle.js
@@ -1,5 +1,10 @@
-$(document).on('click', '.toggle-content-trigger', function() {
+$(document).on('click', '.toggle-content-trigger', function(e) {
  	$(this).toggleClass('open'); // Toggle button to open state
     $(this).parent().siblings('.toggle-content').toggleClass('visible'); // Toggle content visible
  	$(this).siblings('.toggle-content').toggleClass('visible'); // Toggle content visible
+    e.preventDefault();
+});
+
+$('.toggle-content-trigger[data-toggle]').each(function(){
+    $(this).parent().siblings('.toggle-content').addClass($(this).attr('data-toggle'));
 });
