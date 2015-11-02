272fddc339b2709ec5335e441acaabff9ad675d3
Brandon Rosage <brandon.rosage@gmail.com>
2015-11-02 14:35:44 -0600

    REVISION: toggle.js for RTL
    Signed-off-by: Brandon Rosage <brandon.rosage@gmail.com>

--- assets/js/custom/toggle.js
+++ assets/js/custom/toggle.js
@@ -1,33 +1,52 @@
+dropdownXpos = function(trigger, target) {
+   var triggerPos = $(trigger).position();
+
+   // IF: Right-to-left language
+   if ($(trigger).css('direction') == 'rtl') {
+      $(target).css('right', ($(window).width() - (triggerPos.left + $(trigger).outerWidth())));
+   // ELSE:
+   } else {
+      $(target).css('left', triggerPos.left);
+   }
+}
+
 // Initialize each toggle pair
 $('[data-toggle]').each(function(){
-    var trigger = $(this),
-        targetVal = $(trigger).attr('data-toggle'),
-        target = $('#'+targetVal).length ? $('#'+targetVal) : $(trigger).next('.'+targetVal);
+   var trigger = $(this),
+   targetVal = $(trigger).attr('data-toggle'),
+   target = $('#'+targetVal).length ? $('#'+targetVal) : $(trigger).next('.'+targetVal);
 
-    // Add 'init' class to pair of elements
-    $(trigger).addClass('init');
-    $(target).addClass('init');
+   // Add 'init' class to pair of elements
+   $(trigger).addClass('init');
+   $(target).addClass('init');
 
-    // IF: The target is already visible, apply 'active' class to elements
-    if ($(target).is(':visible')) {
-        $(trigger).addClass('active');
-        $(target).addClass('active');
-    }
+   // IF: The target is already visible, apply 'active' class to elements
+   if ($(target).is(':visible')) {
+      $(trigger).addClass('active');
+      $(target).addClass('active');
 
-    // Add 'click' handler to toggle trigger
-    $(trigger).on('click', function(e){
+      if ($(target).hasClass('dropdown-menu')) {
+         dropdownXpos(trigger, target);
+      }
+   }
 
-        // IF: Target is currently hidden
-        if ($(target).is(':hidden')) {
-            $(trigger).addClass('active');
-            $(target).addClass('active');
+   // Add 'click' handler to toggle trigger
+   $(trigger).on('click', function(e){
 
-        // ELSE: Target is currently visible
-        } else {
-            $(trigger).removeClass('active');
-            $(target).removeClass('active');
-        }
+      // IF: Target is currently hidden
+      if ($(target).is(':hidden')) {
+         $(trigger).addClass('active');
+         $(target).addClass('active');
+         if ($(target).hasClass('dropdown-menu')) {
+            dropdownXpos(trigger, target);
+         }
 
-        e.preventDefault();
-    });
+         // ELSE: Target is currently visible
+      } else {
+         $(trigger).removeClass('active');
+         $(target).removeClass('active').removeAttr('style');
+      }
+
+      e.preventDefault();
+   });
 });
