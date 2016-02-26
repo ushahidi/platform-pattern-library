ea86b52acdaa115f248e8f116ed95a5e129a85cd
Seth Hall <seth@ushahidi.com>
2016-02-26 15:19:01 -0500

    Mode Context Menu
    * adds all elements to mode context

---
+++ assets/js/custom/mode.js
@@ -0,0 +1,11 @@
+$('.nav-icon-js').click(function(e){
+    $('.mode-nav').toggleClass('open');
+});
+
+$('.mode-nav .toggle-menu').click(function(e){
+    $('.mode-nav').toggleClass('collapsed');
+});
+
+$('.mode-context .toggle-menu').click(function(e){
+    $('.mode-context').toggleClass('collapsed');
+});
