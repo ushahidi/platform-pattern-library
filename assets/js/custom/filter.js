febcf4c8e66aa4892f5c79ad6c06b522b44598ad
Seth Hall <seth@ushahidi.com>
2016-04-07 14:42:55 -0400

    NEW: Modules View Switcher
    * RTL support
    * still need to work out mobile JS

--- assets/js/custom/filter.js
+++ assets/js/custom/filter.js
@@ -1,21 +1,3 @@
-/*
-// Toggle arrow icon and open filter container
-$('.filter-button-js').click(function() {
-	$(this).toggleClass('open');
-
-    if ( $('.tabs-menu-list').hasClass('visible') ) {
-        $('.tabs-menu-button-js').toggleClass('open');
-        $('.tabs-menu-list').toggleClass('visible');
-    }
-
-    $(this).parent().siblings('.filters-container').toggleClass('visible');
-});
-
-$('.filters-container a').click(function(e) {
-    e.preventDefault();
-});
-*/
-
 $('#filters fieldset').each(function(){
 	var fieldset = $(this),
 		fieldCount = fieldset.find('.form-field').length;
