9a44509535946e4d5fd49e8e6f45dc26bca7c690
Brandon Rosage <brandon.rosage@gmail.com>
2016-04-14 11:05:43 -0500

    REVISION: "Listing" variations and layout
    Signed-off-by: Brandon Rosage <brandon.rosage@gmail.com>

--- assets/js/custom/listing.js
+++ assets/js/custom/listing.js
@@ -2,20 +2,22 @@
 $('.listing').each(function(){
    var context = $(this),
    toolbar = $(context).find('.listing-toolbar'),
-   checkboxes = $(context).find('input[type="checkbox"]');
+   checkboxes = $(context).find('.listing-item-select input[type="checkbox"]'),
+   checkboxes_checked;
 
    // Add 'init' class to the Mode Bar
    $(context).addClass('init');
 
    // Add 'change' handler to checkboxes
    $(checkboxes).on('change', function(e){
+       checkboxes_checked = $(context).find('.listing-item-select input[type="checkbox"]:checked');
 
-       if ($(checkboxes).is(':checked')) {
+       if ($(checkboxes_checked).length) {
            $(context).addClass('toolbar-active');
        } else {
            $(context).removeClass('toolbar-active');
        }
 
-
+       $(toolbar).find('.listing-toolbar-summary .total').text($(checkboxes_checked).length);
    });
 });
