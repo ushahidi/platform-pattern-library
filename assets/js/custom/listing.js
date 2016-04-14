e8a9053b41d2f088456f135eb2594ab34c988b13
Brandon Rosage <brandon.rosage@gmail.com>
2016-04-14 12:50:42 -0500

    REVISION: Settings layouts
    Signed-off-by: Brandon Rosage <brandon.rosage@gmail.com>

--- assets/js/custom/listing.js
+++ assets/js/custom/listing.js
@@ -2,22 +2,31 @@
 $('.listing').each(function(){
    var context = $(this),
    toolbar = $(context).find('.listing-toolbar'),
-   checkboxes = $(context).find('.listing-item-select input[type="checkbox"]'),
-   checkboxes_checked;
+   toggle_checkboxes = $(context).find('.listing-item-secondary input[type="checkbox"]'),
+   select_checkboxes = $(context).find('.listing-item-select input[type="checkbox"]'),
+   select_checkboxes_checked;
 
    // Add 'init' class to the Mode Bar
    $(context).addClass('init');
 
    // Add 'change' handler to checkboxes
-   $(checkboxes).on('change', function(e){
-       checkboxes_checked = $(context).find('.listing-item-select input[type="checkbox"]:checked');
+   $(select_checkboxes).on('change', function(e){
+       select_checkboxes_checked = $(context).find('.listing-item-select input[type="checkbox"]:checked');
 
-       if ($(checkboxes_checked).length) {
+       if ($(select_checkboxes_checked).length) {
            $(context).addClass('toolbar-active');
        } else {
            $(context).removeClass('toolbar-active');
        }
 
-       $(toolbar).find('.listing-toolbar-summary .total').text($(checkboxes_checked).length);
+       $(toolbar).find('.listing-toolbar-summary .total').text($(select_checkboxes_checked).length);
+   });
+
+   $(toggle_checkboxes).on('change', function(e){
+       if ($(this).is(':checked')) {
+           $(this).closest('.listing-item').addClass('active');
+       } else {
+           $(this).closest('.listing-item').removeClass('active');
+       }
    });
 });
