cc396aaa55e7e641f0fa8748739d608a626efbeb
Seth Hall <seth@ushahidi.com>
2016-04-14 13:39:06 -0400

    FIX: improves field adapt/adds interaction to `<textarea>`

--- assets/js/custom/form.js
+++ assets/js/custom/form.js
@@ -1,14 +1,14 @@
 var formField = $('.form-field-adapt')
 
-$('.form-field-adapt input[type="text"]').click(function() {
-    formField.addClass('is-focused');
+$('.form-field-adapt .adapt-field').focus(function() {
+    $(this).parents(formField).addClass('is-focused');
 });
 
-$('.form-field-adapt input[type="text"]').blur(function() {
+$('.form-field-adapt .adapt-field').blur(function() {
 
-    if ($('input[type="text"]').filter(function() { return $(this).val(); }).length > 0) {
-        formField.addClass('is-focused');
+    if ($('.adapt-field').filter(function() { return $(this).val(); }).length > 0) {
+        $(this).parents(formField).addClass('is-focused');
     }  else {
-        formField.removeClass('is-focused');
+        $(this).parents(formField).removeClass('is-focused');
     }
 });
