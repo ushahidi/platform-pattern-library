ec4e4403cea3878a33fe649731825d80fd41394d
Brandon Rosage <brandon.rosage@gmail.com>
2016-09-19 14:57:39 -0500

    NEW: ‘accordion.js’ interaction
    Signed-off-by: Brandon Rosage <brandon.rosage@gmail.com>

--- assets/js/custom/accordion.js
+++ assets/js/custom/accordion.js
@@ -1,5 +1,24 @@
-$('.accordion-menu-trigger').click(function() {
-	var parent = $(this).parents('.accordion-menu-list__item'); // Find the parent item
-	parent.siblings().removeClass('open'); // Close other items
-	parent.toggleClass('open'); // Open parent item
+$('[data-accordion-items]').each(function(){
+    var accordion = $(this),
+        accordionSelector = '.' + accordion.attr('data-accordion-items'),
+        accordionItems = accordion.find(accordionSelector);
+
+    accordionItems.each(function(){
+        $(this).children().not('[data-accordion-trigger]').wrapAll('<div class="accordion-content" style="display:none;" />');
+    });
+
+    accordionItems.first().addClass('active').children('.accordion-content').show();
+
+    accordion.find('[data-accordion-trigger]').on('click', function(e){
+        var accordionItem = $(this).closest(accordionSelector),
+            accordionItemTarget = $(this).attr('data-accordion-trigger') ?
+                accordionItems.eq($(this).attr('data-accordion-trigger')) : accordionItem;
+
+        if (!accordionItemTarget[0].hasAttribute('disabled')) {
+            accordionItems.not(accordionItemTarget).removeClass('active').children('.accordion-content').slideUp();
+            accordionItemTarget.addClass('active').children('.accordion-content').slideDown();
+        }
+
+        e.preventDefault();
+    })
 });
