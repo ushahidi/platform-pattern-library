082d8c99c48fcca18468b40e0085405a12777371
Brandon Rosage <brandon.rosage@gmail.com>
2016-10-06 16:23:10 -0500

    REVISION: Tasks as listing in toolbox
    Signed-off-by: Brandon Rosage <brandon.rosage@gmail.com>

--- assets/js/handlebars/handlebars.helpers.js
+++ assets/js/handlebars/handlebars.helpers.js
@@ -73,9 +73,9 @@ Handlebars.registerHelper('postBand', function() {
     );
 });
 
-Handlebars.registerHelper('postField', function(surveyIndex, postIndex, fieldIndex, value) {
+Handlebars.registerHelper('postcardField', function(surveyIndex, postIndex, fieldIndex, value) {
     var fieldType = session.deployment.surveys[surveyIndex].fields[fieldIndex].type;
-    // return options.fn(session.deployment.surveys[surveyIndex].fields[fieldIndex].type);
+
     if (fieldType == 'photo') {
         return new Handlebars.SafeString(
             '<img src="'+value+'" class="postcard-image" />'
@@ -89,12 +89,92 @@ Handlebars.registerHelper('postField', function(surveyIndex, postIndex, fieldInd
     }
 });
 
-Handlebars.registerHelper('taskName', function(postIndex, taskIndex) {
-    var surveyIndex = session.deployment.responses[postIndex].properties.survey;
-    console.log('survey: ' + surveyIndex + ', task: ' + taskIndex);
-    return new Handlebars.SafeString(
-        session.deployment.surveys[surveyIndex].tasks[taskIndex].name
-    );
+Handlebars.registerHelper('formField', function(surveyIndex, postIndex, fieldIndex, value) {
+    var fieldType = session.deployment.surveys[surveyIndex].fields[fieldIndex].type,
+        fieldLabel = session.deployment.surveys[surveyIndex].fields[fieldIndex].label;
+
+    if (fieldType == 'text') {
+        return new Handlebars.SafeString(
+            '<div class="form-field"><label>'+fieldLabel+'</label><input type="text" value="'+value+'" /></div>'
+        );
+    } else if (fieldType == 'textarea') {
+        return new Handlebars.SafeString(
+            '<div class="form-field"><label>'+fieldLabel+'</label><textarea>'+Handlebars.helpers.striptags(value)+'</textarea></div>'
+        );
+    } else if (fieldType == 'photo') {
+        return new Handlebars.SafeString(
+            '<div class="form-field file"> \
+                <label>' + fieldLabel + '</label> \
+                <figure> \
+                    <img src="' + value + '" /> \
+                    <div class="form-field"> \
+                        <button class="button-destructive"> \
+                            <svg class="iconic"> \
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../../img/iconic-sprite.svg#trash"></use> \
+                            </svg> \
+                            <span class="hidden">Delete photo</span> \
+                        </button> \
+                        <input type="file" id="photo" /> \
+                        <label for="photo" class="button button-gamma"> \
+                            <svg class="iconic"> \
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../../img/iconic-sprite.svg#image"></use> \
+                            </svg> \
+                            <span class="button-label">Replace image</span> \
+                        </label> \
+                    </div> \
+                </figure> \
+            </div>'
+        );
+    } else if (fieldType == 'location') {
+        return new Handlebars.SafeString(
+            '<div class="form-field location"> \
+                <label>' + fieldLabel + '</label> \
+                <div id="map" data-post-index="' + postIndex + '" class="map"></div> \
+                <form role="search" class="searchbar" data-message="search"> \
+                    <div class="searchbar-input"> \
+                        <div class="form-field"> \
+                            <input type="search" maxlength="250" placeholder="Find a location" value="' + value + '" /> \
+                        </div> \
+                        <div class="searchbar-results dropdown-menu"> \
+                            <div class="form-field"> \
+                                <button class="button-plain"> \
+                                    <svg class="iconic"> \
+                                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../../img/iconic-sprite.svg#magnifying-glass"></use> \
+                                    </svg> \
+                                    <span class="button-label">Search for "<em>123 Main St</em>"</span> \
+                                </button> \
+                                <button class="button-beta button-plain"> \
+                                    <svg class="iconic"> \
+                                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../../img/iconic-sprite.svg#location"></use> \
+                                    </svg> \
+                                    <span class="button-label">Use your current location</span> \
+                                </button> \
+                            </div> \
+                            <div class="tool"> \
+                                <h6 class="tool-heading">Search results</h6> \
+                                <dl class="dropdown-menu-body"> \
+                                    <dt class="list-item"><a href="#"><em>123 Main St</em>reet Austin, TX USA</a></dt> \
+                                    <dt class="list-item"><a href="#"><em>123</em> Paint Supply, 416 Lamar Blvd. Austin, TX USA</a></dt> \
+                                </dl> \
+                            </div> \
+                        </div> \
+                    </div> \
+                </form> \
+                <p><em>You can search or click the area of map where you want to place the marker.</em></p> \
+            </div>'
+        );
+    }
+});
+
+Handlebars.registerHelper('taskInfo', function(key, postIndex, taskIndex) {
+    var surveyIndex = session.deployment.responses[postIndex].properties.survey,
+        keyValue = session.deployment.surveys[surveyIndex].tasks[taskIndex][key];
+
+    if (keyValue !== undefined) {
+        return new Handlebars.SafeString(
+            session.deployment.surveys[surveyIndex].tasks[taskIndex][key]
+        );        
+    }
 });
 
 Handlebars.registerHelper('postCheckbox', function() {
