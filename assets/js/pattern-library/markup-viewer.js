ca8afc250a3c12fbe789b97554124dd845b69cf8
Ryan Christoffersen <ryan@ryanchristo.com>
2017-07-04 12:44:45 +0700

    Fixed duplication of pattern sample markup

--- assets/js/pattern-library/markup-viewer.js
+++ assets/js/pattern-library/markup-viewer.js
@@ -1,9 +1,17 @@
 $(function() {
-	$('.pl-sub-pattern-markup').each(function(){
-		$(this).before('<div class="pl-sub-pattern-sample"><div class="pl-sub-pattern-sample-toggle-controls"><span class="show">&#43; Show HTML</span><span class="hide">&#45; Hide HTML</span></div><div class="pl-sub-pattern-sample-toggle-target"><pre class="language-handlebars"><code><script type="prism-html-markup">'+$(this).html()+'</script></code></pre></div></div>');
+
+	$('.pl-sub-pattern-markup').each(function(index){		
+			if (!$('#pattern-sample-' + index)[0]) {
+					$(this).before('<div id="pattern-sample-' + index + '" class="pl-sub-pattern-sample"><div class="pl-sub-pattern-sample-toggle-controls"><span class="show">&#43; Show HTML</span><span class="hide">&#45; Hide HTML</span></div><div class="pl-sub-pattern-sample-toggle-target"><pre class="language-handlebars"><code><script type="prism-html-markup">'+$(this).html()+'</script></code></pre></div></div>');
+			}
 	});
 
-	$('.pl-sub-pattern').on('click','.pl-sub-pattern-sample-toggle-controls', function(){
-		$(this).parent('.pl-sub-pattern-sample').toggleClass('visible');
+	$('.pl-sub-pattern').on('click','.show', function(){
+			$(this).parent().parent().addClass('visible');
 	});
+
+	$('.pl-sub-pattern').on('click','.hide', function(){
+			$(this).parent().parent().removeClass('visible');
+	});
+
 });
